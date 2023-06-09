f4b1719a20e1d996c6afa56bbca96debc8f3f54b
Sthephanie Leon <sleonv@ucenfotec.ac.cr>
2023-06-09 16:07:26 -0600

    Creación de base de datos en Postgrest SQL -tablas, constraints -secuencias -inserts
    Se incluye Clase para establecer la conexion con la base de datos y una clase para probar la conexion

---
+++ src/Backend/Model/dl/TestConexion.java
@@ -0,0 +1,39 @@
+package Backend.Model.dl;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+public class TestConexion {
+
+/*============ESTA CLASE ES SOLAMENTE DE PRUEBA PARA VERIFICAR QUE LA CONEXIÓN A LA BASE DE DATOS FUNIONE CORRECTAMENTE*/
+
+    public static void main(String[] args) {
+        String idCliente = "8";
+        String obtieneNombreCliente = consultaClienteTest("8");
+        System.out.println("nombre del cliente: " + obtieneNombreCliente);
+
+    }
+
+    public static String consultaClienteTest(String idCliente) {
+        ConexionBD con = new ConexionBD();
+        String sql = "SELECT NOMBRE_COMPLETO FROM CLIENTES CL WHERE CL.ID_CLIENTE=" + idCliente + "";
+        String nombre = "";
+        try {
+            Statement st = con.getConnection().createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            if (rs.next()) {
+                nombre = rs.getString(1);
+
+            }
+            con.desconectarBD();//cierre de conexión
+
+        } catch (SQLException ex) {
+            Logger.getLogger(TestConexion.class.getName()).log(Level.SEVERE, null, ex);
+            con.desconectarBD();//cierre de conexión
+            return nombre;
+        }
+        return nombre;
+
+    }
+}
